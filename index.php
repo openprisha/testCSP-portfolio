aa5e12d4f1fea58453ca5f255ab5d3ccbacd6d69
openprisha <parashar.prisha@gmail.com>
2019-12-06 03:20:24 +0000

    updating with substantial changes

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Prisha Parashar: Dublin High School Freshman</title>
+    
+  </head>
+  <body>
+    <?php echo '<p>Prisha Parashar, Period 1 CSP</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Parashar_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Parashar_Projects.html">Projects.html</a></li></br>
+      <li><a href="Parashar_Resume.html">Resume.html</a></li></br>
+      <li><a href="Parashar_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+
+    
+  </body>
+</html>
